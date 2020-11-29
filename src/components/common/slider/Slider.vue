9544f8a934f2ec76f7f971602cfde56ef7dbd79d
1226620975@qq.com <1226620975@qq.com>
2020-11-29 21:48:15 +0800

    轮播图制作第一天

--- src/components/common/slider/Slider.vue
+++ src/components/common/slider/Slider.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="imgBox">
-    <div v-for="item in banners" :key="item">
-      <a :href="item.url">
+  <div class="slider">
+    <div class="left">
+      <div class="prev">
+        <img src="~assets/img/swiper/prev.svg" alt="" />
+      </div>
+    </div>
+    <div class="right">
+      <div class="next">
+        <img src="~assets/img/swiper/next.svg" alt="" />
+      </div>
+    </div>
+    <div class="imgBox">
+      <!-- <div v-for="item in banners" :key="item" class="slider">
+      <a :href="item.url" class="imgSize">
         <img :src="item.imageUrl" />
       </a>
+    </div> -->
     </div>
   </div>
 </template>
